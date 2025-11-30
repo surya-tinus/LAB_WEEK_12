c54481eb7304f8657e85752c9231aedfa0944eac
surya-tinus <suryanovenskytinus@gmail.com>
2025-11-30 15:26:20 +0700

    Commit No. 2 – Building app with coroutines

--- app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
+++ app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.test_lab_week_12
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.test_lab_week_12.model.Movie
+import com.google.android.material.snackbar.Snackbar
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     private val movieAdapter by lazy {
@@ -18,10 +22,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val recyclerView: RecyclerView = findViewById(R.id.movie_list)
         recyclerView.adapter = movieAdapter
+        val movieRepository = (application as MovieApplication).movieRepository
+        val movieViewModel = ViewModelProvider(
+            this, object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return MovieViewModel(movieRepository) as T
+                }
+            })[MovieViewModel::class.java]
+        movieViewModel.popularMovies.observe(this) { popularMovies ->
+            val currentYear =
+                Calendar.getInstance().get(Calendar.YEAR).toString()
+            movieAdapter.addMovies(
+                popularMovies
+                    .filter { movie ->
+// aman dari null
+                        movie.releaseDate?.startsWith(currentYear) == true
+                    }
+                    .sortedByDescending { it.popularity }
+            )
+        }
+        movieViewModel.error.observe(this) { error ->
+            if (error.isNotEmpty()) {
+                Snackbar.make(recyclerView, error, Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
+
 
     private fun openMovieDetails(movie: Movie) {
         val intent = Intent(this, DetailsActivity::class.java).apply {
